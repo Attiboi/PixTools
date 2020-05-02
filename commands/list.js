94c2f3200ed7e62a96825b7bc6e3cdf8d9ef6e27
Attiboi <64702263+Attiboi@users.noreply.github.com>
2020-05-02 14:59:00 -0600

    List.js
    Gives a list of all the games.

---
+++ commands/list.js
@@ -0,0 +1,56 @@
+module.exports = {
+  name: 'online',
+  description: 'Returns the games on hypixel.',
+  message.channel.send('1. Battle Royale 
+2. Hide and Seek 
+3. The Bridge 
+4. The Pit 
+5. Bedwars
+6. Skywars
+7. Build Battle Solo
+8. Build Battle Teams
+9. Guess The Build
+10. Build Battle - Pro Mode (Beta)
+11. Turbo Kart Racers
+12. The Walls
+13. Arena Brawl
+14. Paintball
+15. Quakecraft
+16. VampireZ
+17. Murder Mystery
+18. UHC Duels
+19. Skywars Duels
+20. Duels
+21. OP Duels
+22. Duel Tournament
+23. Smash Heros
+24. Speed UHC
+25. Warlords
+26. Crazy Walls
+27. Cops and Crims
+28. Mini Walls
+29. UHC Champions
+30. Blitz Survival Games
+31. Mega Walls
+32. Sky Clash
+33. Player Housing
+34. PvP Run
+35. TNT Run
+36. Bow Spleef
+37. TNT Tag
+38. TNT Wizards
+39. Blocking Dead
+40. Hypixel/Santa Says
+41. Farm Hunt
+42. Creeper Attack
+43. Party Games 1, 2 and 3
+44. Hole In The Wall
+45. FootBall
+46. Pixel Painters
+47. Bounty Hunters
+48. Dragon Wars
+49. Ender Spleef
+50. Galaxy Wars
+51. Throw Out
+52. Zombies') 
+}
